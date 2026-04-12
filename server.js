fc707d69aa1c4ff4a2ce596b27d114d7f5cbab46
Kamil Korzeniowski <kamil.korzeniowski.fr@gmail.com>
2026-04-12 20:29:20 +0200

    Prepare for render

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Base de données simulée
 let tasks = [{ id: 1, name: "Exemple de tâche", completed: false, createdAt: new Date().toISOString(), deadline: null }];
